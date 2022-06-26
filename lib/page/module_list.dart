d16eef0044e0621e73f9bce2e59ea8687fdca293
Aaron <aaronevanjulio18@gmail.com>
2022-06-26 19:53:38 +0700

    Initial commit

---
+++ lib/page/module_list.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sm_setstate/page/done_module_provider.dart';
+
+import 'module_tile.dart';
+
+class ModuleList extends StatelessWidget {
+  final List<String> moduleList = [
+    'Modul 1 - Pengenalan Dart',
+    'Modul 2 - Memulai Pemrograman dengan Dart',
+    'Modul 3 - Dart Fundamental',
+    'Modul 4 - Control Flow',
+    'Modul 5 - Collections',
+    'Modul 6 - Object Oriented Programming',
+    'Modul 7 - Functional Styles',
+    'Modul 8 - Dart Type System',
+    'Modul 9 - Dart Futures',
+    'Modul 10 - Effective Dart',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: moduleList.length,
+      itemBuilder: (context, index) {
+        return Consumer<DoneModuleProvider>(
+          builder: (context, DoneModuleProvider data, widget){
+            return ModuleTile(
+              moduleName: moduleList[index],
+              isDone: data.doneModuleList.contains(
+                moduleList[index]
+              ),
+              onClick: () {
+                data.complete(moduleList[index]);
+              },
+            );
+          }
+        );
+      },
+    );
+  }
+}
---
+++ lib/page/module_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'done_module_list.dart';
+import 'module_list.dart';
+
+class ModulePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Memulai Pemrograman Dengan Dart'),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DoneModuleList(),
+                ),
+              );
+            },
+            icon: const Icon(Icons.done),
+          ),
+          const SizedBox(width: 20,)
+        ],
+      ),
+      body: ModuleList(),
+    );
+  }
+}
